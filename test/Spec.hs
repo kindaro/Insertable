850e7fcef52bcce587c2d558e5b5661926afac49
Ignat Insarov <kindaro@gmail.com>
2017-01-22 06:34:04 +0300

    Add a test from the top of my head.
    It actually tests nothing. I will have to think hard on how to generate
    test cases.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,32 @@
+
+module Main where
+
+import Test.QuickCheck
+
+import Data.Insertable
+
+instance Arbitrary Armoury where
+    arbitrary = do
+        m <- choose (0,96) :: Gen Integer
+        n <- choose (0,96) :: Gen Integer
+        return $ Armoury m n
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    quickCheck propMinBound
+    quickCheck propMaxBound
+
+propMinBound :: [Armoury] -> Property
+propMinBound xs =
+    let z = xs #> (mempty :: Armoury) in
+    not (null xs) ==> daggers z >= 0 && swords z >= 0
+
+propMaxBound :: [Armoury] -> Property
+propMaxBound xs =
+    let
+        z = xs #> (mempty :: Armoury)
+        l = fromIntegral . length $ xs
+    in
+        True ==> daggers z <= (l * 96) && swords z <= (l * 96)
+
